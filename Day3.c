dadbcf03de362c5d74be992c7c516a3d0d2f329c
Kavya Agrawal <15kavyaagrawal@gmail.com>
2026-02-03 22:47:28 +0530

    Add files via upload

---
+++ Day3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, k, i;
+    int count = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter key to search: ");
+    scanf("%d", &k);
+
+    for(i = 0; i < n; i++)
+    {
+        count++;                 // comparison made
+        if(arr[i] == k)
+        {
+            printf("Found at index %d\n", i);
+            printf("Comparisons = %d", count);
+            return 0;
+        }
+    }
+
+    // If not found
+    printf("Not Found\n");
+    printf("Comparisons = %d", count);
+
+    return 0;
+}
