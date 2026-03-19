a4c3bdc9e2600f3633ec31f46b8bd38b6bd4fe95
Kavya Agrawal <15kavyaagrawal@gmail.com>
2026-03-19 22:16:22 +0530

    Add files via upload

---
+++ Day42.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define MAX 100
+
+int queue[MAX];
+int front = 0, rear = -1;
+
+int stack[MAX];
+int top = -1;
+
+void enqueue(int x) {
+    rear++;
+    queue[rear] = x;
+}
+
+int dequeue() {
+    return queue[front++];
+}
+
+void push(int x) {
+    stack[++top] = x;
+}
+
+int pop() {
+    return stack[top--];
+}
+
+int main() {
+    int n, i, x;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    for(i = 0; i < n; i++) {
+        push(dequeue());
+    }
+
+    front = 0;
+    rear = -1;
+
+    for(i = 0; i < n; i++) {
+        enqueue(pop());
+    }
+
+    for(i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+
+    return 0;
+}
